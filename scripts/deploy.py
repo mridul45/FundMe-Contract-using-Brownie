28d4ef17c025d78db141de1fab0a37df4994e54c
Mridul Singhal <73380291+mridul45@users.noreply.github.com>
2022-08-13 18:13:07 +0530

    Add files via upload

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,35 +1,28 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
-from web3 import Web3
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
-
     account = get_account()
-
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
-            "eth-usd-price-feed"
+            "eth_usd_price_feed"
         ]
-
     else:
-        print(f"Active netork is {network.show_active()}")
-        print("Deploying Mock...")
-
-		if len(MockV3Aggregator)<=0:
-			mock_aggregator = MockV3Aggregator.deploy(
-				2000, Web3.toWei("ether"), {"from": account}
-			)
-
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
-        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        price_feed_address,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
